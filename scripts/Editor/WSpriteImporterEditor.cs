25cc9b9dad9cb9b19788f04fc766676d89c493e7
jim <irawan.jemmy@gmail.com>
2020-11-03 15:28:41 +0800

    added sprite importer

---
+++ scripts/Editor/WSpriteImporterEditor.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Wowsome {
+  using EU = EditorUtils;
+  using TextureType = WSpriteImporter.TextureType;
+
+  public static class TextureImporterExt {
+    public static string PlatformAndroid() {
+      // well done unity! keep changing everything!
+#if UNITY_2018_1_OR_NEWER
+      return "android";
+#else
+      return "Android";
+#endif
+    }
+
+    public static string PlatformIos() {
+      // well done unity! keep changing everything!
+#if UNITY_2018_1_OR_NEWER
+      return "ios";
+#else
+      return "iPhone";
+#endif
+    }
+
+    public static bool GetOriginalImageSize(this TextureImporter importer, out int width, out int height) {
+      if (importer != null) {
+        object[] args = new object[2] { 0, 0 };
+        MethodInfo mi = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+        mi.Invoke(importer, args);
+
+        width = (int)args[0];
+        height = (int)args[1];
+
+        return true;
+      }
+
+      height = width = 0;
+      return false;
+    }
+
+    public static void OverridePlatformSettings(this TextureImporter importer, string platform, TextureImporterFormat format, int maxSize) {
+      TextureImporterPlatformSettings settings = importer.GetPlatformTextureSettings(platform);
+      settings.overridden = true;
+      settings.format = format;
+      settings.maxTextureSize = maxSize;
+      importer.SetPlatformTextureSettings(settings);
+    }
+
+    /// <summary>
+    /// Post processor of texture of the background stuff.
+    /// 1. ios, Set max size to 2048 format RGB 16 bit
+    /// 2. android, Set max size to 1028 format RGB 16 bit
+    /// </summary>    
+    public static void SetBackgroundSettings(this TextureImporter importer) {
+      // android
+      importer.OverridePlatformSettings(PlatformAndroid(), TextureImporterFormat.RGB16, 1024);
+      // ios
+      importer.OverridePlatformSettings(PlatformIos(), TextureImporterFormat.RGB16, 2048);
+      // apply changes
+      EditorUtility.SetDirty(importer);
+      importer.SaveAndReimport();
+    }
+
+    /// <summary>
+    /// The Post Processor for the common images. How it works:
+    /// 1. Get the original image dimensions e.g 400x200
+    /// 2. Get the max size of width and height (in this case 400), then get previous power of two of it i.e 256
+    /// 3. For android set max size to the POT value, set format to ASTC 8x8
+    /// 4. For ios set max size to POT * 2, set format to ASTC 8x8
+    /// </summary>    
+    public static void SetCommonSettings(this TextureImporter importer) {
+      int w, h;
+      importer.GetOriginalImageSize(out w, out h);
+      int prevPowerOfTwo = System.Math.Max(w, h).PrevPowerOfTwo();
+      // android
+      importer.OverridePlatformSettings(PlatformAndroid(), TextureImporterFormat.ASTC_RGBA_8x8, prevPowerOfTwo);
+      // ios 
+      importer.OverridePlatformSettings(PlatformIos(), TextureImporterFormat.ASTC_RGBA_8x8, prevPowerOfTwo * 2);
+
+      EditorUtility.SetDirty(importer);
+      importer.SaveAndReimport();
+    }
+  }
+
+  [CustomEditor(typeof(WSpriteImporter))]
+  public class WSpriteImporterEditor : Editor {
+    delegate void TexturePostProcessor(TextureImporter importer);
+
+    Dictionary<TextureType, TexturePostProcessor> m_postProcessors = new Dictionary<TextureType, TexturePostProcessor>(){
+      {TextureType.Common, (importer)=> importer.SetCommonSettings()},
+      {TextureType.Background, (importer)=> importer.SetBackgroundSettings()},
+    };
+
+    public override void OnInspectorGUI() {
+      DrawDefaultInspector();
+      WSpriteImporter tgt = (WSpriteImporter)target;
+
+      EU.VPadding(() => {
+        tgt.Data.ForEach(d => {
+          if (GUILayout.Button("PACK " + d.FolderPaths)) {
+            d.Folders.ForEach(f => {
+              string path = d.Path + "/" + f;
+              string[] filePaths = Directory.GetFiles(path);
+              foreach (string fName in filePaths) {
+                if (fName.EndsWithMulti(new List<string> { "png", "jpg" })) {
+                  TextureImporter textureImporter = (TextureImporter)AssetImporter.GetAtPath(fName);
+                  m_postProcessors[d.Type](textureImporter);
+                }
+              }
+            });
+          }
+
+          EditorUtils.VSpacing();
+        });
+      });
+    }
+  }
+}
---
+++ scripts/WSpriteImporter.cs
@@ -0,0 +1,50 @@
+﻿#if UNITY_EDITOR
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wowsome {
+  /// <summary>
+  /// Attach this script to a gameobject prefab, then click Pack button on the Editor 
+  /// to automagically pack all the sprites defined in the Data list.
+  /// due to sprite atlas causes problem, right now it does not pack it into atlas, 
+  /// but rather just set the texture settings on click Pack button according to the TextureType  
+  /// </summary>
+  public class WSpriteImporter : MonoBehaviour {
+    // TODO: might want to add more type later, or at least make this customizable.
+    [Serializable]
+    public enum TextureType {
+      Common,
+      Background
+    }
+
+    /// <summary>
+    /// The Sprite Packer Data
+    /// It consists of the main path as well as one or more folders.    
+    /// </summary>
+    [Serializable]
+    public class SpritePackerData {
+      /// <summary>
+      /// The main path 
+      /// e.g. Assets/sprites
+      /// </summary>
+      public string Path;
+      /// <summary>
+      /// You can define one or more folder path here
+      /// </summary>
+      public List<string> Folders;
+      public TextureType Type;
+
+      public string FolderPaths {
+        get {
+          return Folders.Fold(string.Empty, (prev, cur) => string.Format("{0}/{1}", prev, cur));
+        }
+      }
+    }
+
+    public List<SpritePackerData> Data = new List<SpritePackerData>();
+  }
+}
+
+#endif
